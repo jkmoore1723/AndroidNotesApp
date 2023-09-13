fbf50ff6595c707ba1f34ac181427d8e68f80473
Jkmoore <96745758+jkmoore1723@users.noreply.github.com>
2023-09-13 16:08:32 -0400

    comment out completehooks
    So allure reports can generate from jenkins

--- wdio.conf.js
+++ wdio.conf.js
@@ -306,25 +306,25 @@ export const config = {
     //==============================================
     // onComplete hooks for auto-gen Allure reporting
     //==============================================
-    onComplete: function() { const reportError = new Error('Could not generate Allure report')
-    const generation = allure(['generate', 'allure-results', '--clean'])
-    return new Promise((resolve, reject) => {
-        const generationTimeout = setTimeout(
-            () => reject(reportError),
-            5000)
+    // onComplete: function() { const reportError = new Error('Could not generate Allure report')
+    // const generation = allure(['generate', 'allure-results', '--clean'])
+    // return new Promise((resolve, reject) => {
+    //     const generationTimeout = setTimeout(
+    //         () => reject(reportError),
+    //         5000)
 
-        generation.on('exit', function(exitCode) {
-            clearTimeout(generationTimeout)
+    //     generation.on('exit', function(exitCode) {
+    //         clearTimeout(generationTimeout)
 
-            if (exitCode !== 0) {
-                return reject(reportError)
-            }
+    //         if (exitCode !== 0) {
+    //             return reject(reportError)
+    //         }
 
-            console.log('Allure report successfully generated')
-            resolve()
-        })
-    })
-    },
+    //         console.log('Allure report successfully generated')
+    //         resolve()
+    //     })
+    // })
+    // },
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
